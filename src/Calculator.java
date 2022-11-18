5e6c4870784b0f0f54a779a419d8672d376f5cc0
Vyacheslav Nechepurenko <masterdog8@mail.ru>
2022-11-18 20:17:21 +0300

    initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,29 @@
+import java.util.function.*;
+
+public class Calculator {
+
+    static Supplier<Calculator> instance = Calculator::new;
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    //BinaryOperator<Integer> devide = (x, y) -> x / y; // деление на ноль недопустимо
+    BinaryOperator<Integer> division = (x, y) -> y / x; // реализация аналога мат операции над двумя числами
+
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+
+    Predicate<Integer> isPositive = x -> x > 0;
+
+    Consumer<Integer> println = System.out::println;
+
+    public static void main(String[] args) {
+
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1,1);
+        int c = calc.division.apply(a, b);
+        calc.println.accept(c);
+    }
+}
