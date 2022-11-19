cd12efe7cbd01bc416bb7ff888b85c5167c66ab0
Vyacheslav Nechepurenko <masterdog8@mail.ru>
2022-11-19 20:45:20 +0300

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,6 @@ public class Main {
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
         int c = calc.division.apply(a, b);
-        calc.println.accept(c);
+        calc.println.accept(Integer.MAX_VALUE); // возвращение невероятного числа
     }
 }
